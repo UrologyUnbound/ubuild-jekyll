1eee682888ac29c692b9d76f676ca10fd34f5059
naturallyAsh <ash.wil.1990@gmail.com>
2021-11-12 23:22:29 -0500

    merge

---
+++ assets/js/main.js
@@ -0,0 +1,69 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     'use strict';
+
+//     var body = document.querySelector("body");
+
+//     /* ============================
+//     // Smooth scrolling to section
+//     ============================ */
+//     document.querySelectorAll("#ni button").forEach(anchor => {
+//         anchor.addEventListener("click", function (e) {
+//             e.preventDefault();
+        
+//             document.querySelector(this.getAttribute("href")).scrollIntoView({
+//             behavior: "smooth"
+//             });
+//         });
+//     });
+// });
+// });
+// $(document).ready(function(){
+//     // Add smooth scrolling to all links
+//     $("a").on('click', function(event) {
+  
+//       // Make sure this.hash has a value before overriding default behavior
+//       if (this.hash !== "") {
+//         // Prevent default anchor click behavior
+//         event.preventDefault();
+  
+//         // Store hash
+//         var hash = this.hash;
+  
+//         // Using jQuery's animate() method to add smooth page scroll
+//         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+//         $('html, body').animate({
+//           scrollTop: $(hash).offset().top
+//         }, 800, function(){
+  
+//           // Add hash (#) to URL when done scrolling (default click behavior)
+//           window.location.hash = hash;
+//         });
+//       } // End if
+//     });
+  
+   $(document).ready(function(){
+    // Add scrollspy to <body>
+    $('body').scrollspy({target: ".navbar", offset: 50});   
+  
+    // Add smooth scrolling on all links inside the navbar
+    $("#navbarNav a").on('click', function(event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      }  // End if
+    });
+  });
